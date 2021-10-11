0cb01a9b523942d2d0f7ce36d21bd9d70e5ba695
TeeusZs <susksotaku@gmail.com>
2021-10-11 19:00:36 -0300

    make it better

---
+++ main.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext import commands
+import os
+
+intents = discord.Intents.default()
+intents.members = True
+
+testing = False
+
+client = commands.Bot(command_prefix = "!!", case_insensitive = True, intents=intents)
+
+client.remove_command('help')
+
+@client.event
+async def on_ready():
+  print('Entrando como {0.user}'.format(client))
+
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        client.load_extension(f'cogs.{filename[:-3]}')
+
+client.run('ODk2ODc3OTY1MzgyNjUxOTA1.YWNgsg.rwZL0AFtEs5J_Ho5eOKrKN1qhIg')
